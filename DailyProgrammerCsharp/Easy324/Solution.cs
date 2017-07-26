fbbd195641eec1e2c843926c10a6cf884c358524
tommoyang <tommoyang@hotmail.com>
2017-07-26 15:56:44 +1200

    Add project files.

---
+++ DailyProgrammerCsharp/Easy324/Solution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace DailyProgrammerCsharp.Easy324
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Precision: ");
+                var precision = int.Parse(Console.ReadLine());
+
+                Console.Write("Input: ");
+                var input = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Output: " + SqRootAndRound(input, precision));
+
+                Console.WriteLine("Exit? y/n");
+            } while (Console.ReadLine() != "y");
+        }
+
+        public static string SqRootAndRound(double input, int precision)
+        {
+            var result = SqRoot(input);
+
+            return Round(result, precision);
+        }
+
+        // https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+        public static double SqRoot(double input)
+        {
+            int precision = input.ToString().Length;
+
+            double x = precision * 100;
+
+            for (var i = 0; i < (precision + 1); i++)
+            {
+                x = (x + input / x) / 2;
+            }
+
+            return x;
+        }
+
+        public static string Round(double input, int precision)
+        {
+            var inputStr = input.ToString();
+
+            var dp = inputStr.IndexOf(".");
+
+            var length = dp > 0 ? dp : inputStr.Length;
+
+            if (dp > 0 && precision > 0)
+            {
+                length += precision + 1;
+            }
+
+            var modified = inputStr;
+
+            if (inputStr.Length < length)
+            {
+                modified = modified.PadRight(length - modified.Length).Replace(" ", "0");
+            }
+            else
+            {
+                modified = modified.Substring(0, length);
+            }
+
+            return modified;
+        }
+    }
+}
---
+++ DailyProgrammerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DailyProgrammerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DailyProgrammerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4bb0d201-0d84-4742-9aaf-5acce9ee3525")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
